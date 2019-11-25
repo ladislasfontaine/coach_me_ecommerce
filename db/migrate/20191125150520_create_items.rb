3a908696a41489e31c8c9faebb13e70599033de7
remi <rr03ml@gmail.com>
2019-11-25 17:56:35 +0100

    Description

--- db/migrate/20191125150520_create_items.rb
+++ db/migrate/20191125150520_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :title
-      t.text :descripton
+      t.text :description
       t.decimal :price
       t.string :image_url
       t.belongs_to :categories, index: true
