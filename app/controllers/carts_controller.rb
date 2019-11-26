a123dbeb0aa115fb867f3c4bf1c45503167f4a31
Margad Gantuya <majagantuya@gmail.com>
2019-11-26 16:50:10 +0100

    create controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,12 @@
 class CartsController < ApplicationController
-end
+##	before_action :authenticate_user, only: [ :index , :update , :delete_item]
+
+##	def update
+		##@cart_item = CartItem.new(item_id: params[:id], cart_id: current_user.cart.id)
+		##if @cart_item.save
+
+			
+		##end
+	##end
+
+end # class 
