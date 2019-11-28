205dfbe7a948c41874195ed6d9db1a4c8decf3ca
Margad Gantuya <majagantuya@gmail.com>
2019-11-28 17:57:19 +0100

    email admin ok

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,14 @@
 class AdminMailer < ApplicationMailer
-end
+	default from: 'no-reply@coach-me.fr'
+
+	def admin_validate(user, admin)
+		@admin = admin
+		@user = user
+		@url = 'https://coach-me-master.herokuapp.com/users/sign_in'
+		mail(to: @admin.email, subject: "Admin validation d'achat")
+		
+	end
+
+
+end # class
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,17 @@
 class Order < ApplicationRecord
   after_create :create_order_items
+  after_create :admin_validate
 
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
   #wait for stripe
+
+  def admin_validate
+    User.where("is_admin", "true").each do |admin|
+      AdminMailer.admin_validate(user,admin).deliver_now
+    end
+  end
 
   def create_order_items
     # creation of order items
